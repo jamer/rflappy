61c4621f20d9947d126f6b37b315d1f20e687231
Paul Merrill <pdm@pdm.me>
2014-05-01 23:26:56 -0700

    draw a green circle

--- window.rs
+++ window.rs
@@ -1,6 +1,6 @@
 // vi: ts=4 sw=4
 
-use rsfml::graphics::{RenderWindow, Color};
+use rsfml::graphics::{RenderWindow, Color, CircleShape};
 use rsfml::window::{VideoMode, ContextSettings, event, keyboard, Close};
 
 pub struct Window {
@@ -10,7 +10,6 @@ pub struct Window {
 
 impl Window {
 	pub fn new(width: uint, height: uint) -> Window {
-		// Create the window of the application
 		let setting : ContextSettings = ContextSettings::default();
 		let mut renderWindow : RenderWindow =
 			match RenderWindow::new(VideoMode::new_init(width, height, 32),
@@ -28,25 +27,42 @@ impl Window {
 		}
 	}
 
-	pub fn event_loop(&mut self) {
+	pub fn event_loop(&mut self) -> () {
 		while self.window.is_open() {
-			loop {
-				match self.window.poll_event() {
-					event::Closed => self.window.close(),
-					event::KeyPressed{code, ..} => match code {
-						keyboard::Escape        => {self.window.close(); break},
-						keyboard::Space         => {},
-						_                       => {}
-					} ,
-					event::NoEvent => break,
-					_ => {}
-				}
-			}
-			// Clear the window
-			self.window.clear(&Color::new_RGB(50, 200, 50));
-
-			// Display things on screen
-			self.window.display()
+			self.pump_events();
+			self.draw();
 		}
+	}
+
+	fn pump_events(&mut self) -> () {
+		loop {
+			match self.window.poll_event() {
+				event::NoEvent => { break }
+				e => { self.handle_event(e) }
+			}
+		}
+	}
+
+	fn handle_event(&mut self, e: event::Event) -> () {
+		match e {
+			event::Closed => {self.window.close();},
+			event::KeyPressed{code, ..} => match code {
+				keyboard::Escape => {self.window.close();},
+				keyboard::Space  => {},
+				_                => {}
+			} ,
+			_ => {}
+		}
+	}
+
+	fn draw(&mut self) -> () {
+		let mut shape: CircleShape = match CircleShape::new_init(100.0, 30) {
+			Some(shape) => shape,
+			None => fail!("Cannot create Circle Shape."),
+		};
+		shape.set_fill_color(&Color::green());
+		self.window.clear(&Color::black());
+		self.window.draw(&shape);
+		self.window.display()
 	}
 }
